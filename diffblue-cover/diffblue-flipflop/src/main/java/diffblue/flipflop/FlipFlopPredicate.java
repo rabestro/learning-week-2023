23046d831aa1284d738a794840bca94b025756a8
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-12-02 20:59:01 +0200

    Add FlipFlopPredicate module and upgrade Java version
    Introduced a new module named 'diffblue-flipflop' to the build. This module includes the FlipFlopPredicate class and its test cases which emulate flip-flop logic similar to two-dots flip-flop in Perl or Ruby. Also updated the Java version used in the project to Java 17. Updated the Junit dependency to 'junit-jupiter', and added a new dependency 'mockito-core' for mocking in tests. The new module and dependencies have been added to enhance the project's functionality and testing capabilities.

---
+++ diffblue-cover/diffblue-flipflop/src/main/java/diffblue/flipflop/FlipFlopPredicate.java
@@ -0,0 +1,26 @@
+package diffblue.flipflop;
+
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Emulates flip-flop logic similar to two-dots flip-flop in Perl or Ruby.
+ */
+public final class FlipFlopPredicate<T> implements Predicate<T> {
+    private final Predicate<? super T> startPredicate;
+    private final Predicate<? super T> endPredicate;
+    private boolean state;
+
+    public FlipFlopPredicate(Predicate<? super T> lhs, Predicate<? super T> rhs) {
+        this.startPredicate = requireNonNull(lhs);
+        this.endPredicate = requireNonNull(rhs);
+    }
+
+    @Override
+    public boolean test(final T value) {
+        var result = state || startPredicate.test(value);
+        state = result && !endPredicate.test(value);
+        return result;
+    }
+}
