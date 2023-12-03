6747d45783f6b2b63b903ceb74105b2f0bf4e0bc
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-12-03 15:54:13 +0200

    Implement Roman to Arabic number conversion
    Replaced thrown UnsupportedOperationException in RomanToArabicConverter.java with a functional method that converts Roman numerals to Arabic numbers. The logic switches based on the passed Roman symbols and calculates the proper Arabic numbers based on the Roman numeral rules. Removed @PendingFeature annotation in the test file as this feature is no longer pending.

--- github-copilot/copilot-roman/src/main/java/copilot/roman/RomanToArabicConverter.java
+++ github-copilot/copilot-roman/src/main/java/copilot/roman/RomanToArabicConverter.java
@@ -7,6 +7,20 @@ public final class RomanToArabicConverter implements ToIntFunction<String> {
 
     @Override
     public int applyAsInt(String value) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        int result = 0;
+        for (int i = value.length(); i-- > 0; ) {
+            char symbol = value.charAt(i);
+            result += switch (symbol) {
+                case 'I' -> result >= 5 ? -1 : 1;
+                case 'V' -> 5;
+                case 'X' -> result >= 50 ? -10 : 10;
+                case 'L' -> 50;
+                case 'C' -> result >= 500 ? -100 : 100;
+                case 'D' -> 500;
+                case 'M' -> 1000;
+                default -> throw new IllegalArgumentException("Unexpected value: " + symbol);
+            };
+        }
+        return result;
     }
 }
